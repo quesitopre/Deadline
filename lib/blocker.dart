e4546c6c41c19184fc2b75c725ffa2300e5c4093
quesitopre <citlallimartinez122@gmail.com>
2026-04-03 08:25:53 -0700

    Implemented List view to display apps availble to block or unblock.

--- lib/blocker.dart
+++ lib/blocker.dart
@@ -1,4 +1,5 @@
 import 'package:deadline_app/styled_page_name.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 class AppBlocker extends StatelessWidget{
@@ -7,10 +8,17 @@ const AppBlocker({super.key});
 
   @override 
   Widget build(context){
-    return Scaffold(
-      //appBar: StyledPageName(pageTitle:"App Blocker" ),
-
-
+    return ListView.separated(
+      itemCount: 6,
+      itemBuilder: (BuildContext context, int index){
+        return ListTile(
+         title: const Text('Block List'),
+         tileColor: Colors.orange.shade50,
+         onTap:(){},
+         );
+      },
+      separatorBuilder: (context, index) => const Divider(color: Colors.white,
+      ),
     );
   }
 }
