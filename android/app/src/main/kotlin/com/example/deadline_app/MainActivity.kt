94b7316059db53b17207f94a54a2c6239cae234a
quesitopre <citlallimartinez122@gmail.com>
2026-04-28 14:26:14 -0700

    Update to store and check blocked list.

--- android/app/src/main/kotlin/com/example/deadline_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deadline_app/MainActivity.kt
@@ -7,16 +7,28 @@ import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity(){
     private val CHANNEL = "deadline_app/blocker"
+    private var blockedApps: List<String> = emptyList()
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                if (call.method == "updateBlockedApps") {
+                    blockedApps = call.argument<List<String>>("apps") ?: emptyList()
+                    result.success(null)
+                }
+            }
 }
 override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         if (intent.action == "SHOW_OVERLAY") {
             val appName = intent.getStringExtra("appName") ?: return
-            MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL)
-                .invokeMethod("showOverlay", mapOf("appName" to appName))
+
+            //show overlay if app is on blocked list
+            if (blockedApps.contains(appName)){
+                MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL)
+                    .invokeMethod("showOverlay", mapOf("appName" to appName))
+            }
         }
     }
 }
