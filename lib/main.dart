9a062e2de881801f072671586f05044903658401
quesitopre <citlallimartinez122@gmail.com>
2026-04-28 11:37:41 -0700

    Update main with focus_overlay.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 //dashboard style sheet
 import 'package:deadline_app/homepage.dart';
-import 'package:_overlay_window.dart';
+import 'package:deadline_app/focus_overlay.dart';
 void main() {
   runApp(
      MaterialApp(
       // Scaffold(
        //body: GradientContainer(),
-        home: Homepage(),
-        ),
-     // ),
-    );// func. used to run app.
+        home: Homepage()),
+         );// func. used to run app.
 }
 // overlay entry point
 @pragma("vm:entry-point")
 void overlayMain() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Material(child: Text("My overlay"))
+    home: FocusOverlay(),
   ));
 }
